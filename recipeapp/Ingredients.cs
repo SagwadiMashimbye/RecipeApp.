45de8758f847560219e87b86429c5b596a1a39a7
Sagwadi Mashimbye <ST10168528@rcconnect.edu.za>
2024-04-09 17:50:37 +0000

    added classes added switches added getters and setters added array list

---
+++ recipeapp/Ingredients.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeApp_
+{
+    internal class Ingredients
+    {
+        public String Name {  get; set; }
+        public double Quantity { get; set; }
+        public String UnitofMeasurement { get; set; }
+
+        public Ingredients(String name, double quantity, String unitofmeasurement) { 
+        
+            Name = name;
+            Quantity = quantity;
+            UnitofMeasurement = unitofmeasurement;
+        }
+    }
+    class RecipeSteps
+    {
+        public String Descripion { get; set; }
+
+        public RecipeSteps(String descripion)
+        {
+            Descripion = descripion;
+        }
+    }
+    class Application
+    {
+        private static List<Ingredients> ingredients = new List<Ingredients>();
+        private static List<RecipeSteps> recipeSteps = new List<RecipeSteps>();
+        private static List<Ingredients> original = new List<Ingredients>();
+
+        public static void Recipe() { }
+
+
+    }
+}
--- recipeapp/Program.cs
+++ recipeapp/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RecipeApp_;
+using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace RecipeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to My Recipe App!!");
+            while (true) {
+                Console.Write("Choose option:\n" +
+                "1. Add Recipe\n" +
+                "2. Display Recipe\n" +
+                "3. Scale Recipe\n" +
+                "4. Reset Quantity\n" +
+                "5. Clear All Data\n Option: ");
+
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        Application.Recipe();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                        
+                }
+            }
+            
+        }
+    }
+}
