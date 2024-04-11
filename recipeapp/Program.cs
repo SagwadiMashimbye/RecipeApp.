b90705b0ac92f35e5fdb255c94fa6549b6d87d5f
Sagwadi Mashimbye <ST10168528@rcconnect.edu.za>
2024-04-11 17:52:29 +0000

    added try and catch

--- recipeapp/Program.cs
+++ recipeapp/Program.cs
@@ -12,8 +12,11 @@ namespace RecipeApp
         {
             int input;
             Console.WriteLine("Welcome to My Recipe App!!");
-            do  {
-                Console.Write("Choose option:\n" +
+            do  
+            {
+                try
+                {
+                    Console.Write("Choose option:\n" +
                 "1. Add Recipe\n" +
                 "2. Display Recipe\n" +
                 "3. Scale Recipe\n" +
@@ -21,33 +24,49 @@ namespace RecipeApp
                 "5. Clear All Data\n" +
                 "6. Close the app\nOption: ");
 
-                input = Convert.ToInt32(Console.ReadLine());
-                switch (input)
-                {
-                    case 1:
-                        Application.Recipe();
-                        break;
-                    case 2:
-                       Application.DisplayRecipe();
-                        break;
-                    case 3:
-                        Application.ScaleRecipe();
-                        break;
-                    case 4:
-                        Application.ResetQuantity();
-                        break;
-                    case 5:
-                        Application.ClearAllData();
-                        break;
-                    case 6:
-                        Console.WriteLine("Thank you for using the app");
-                        break;
-                    default:
-                        Console.WriteLine("Invalid Option");
-                        break;
-                        
+                    input = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (input)
+                    {
+                        case 1:
+                            Application.Recipe();
+                            break;
+                        case 2:
+                            Application.DisplayRecipe();
+                            break;
+                        case 3:
+                            Application.ScaleRecipe();
+                            break;
+                        case 4:
+                            Application.ResetQuantity();
+                            break;
+                        case 5:
+                            Application.ClearAllData();
+                            break;
+                        case 6:
+                            Console.WriteLine("Thank you for using the app");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Option");
+                            break;
+
+                    }
                 }
-            } while (input != 6);
+                catch (FormatException)
+                {
+
+                    Console.WriteLine("Invalid Option");
+                    input = -1;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Invalid Option");
+                    input = -1;
+                }
+               
+                
+            }
+            while (input != 6);
 
         }
     }
